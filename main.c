946e0eef064169a878c3d6fe4d5b4e684ff498bf
David Sanford <dsanfordseu@gmail.com>
2016-08-22 23:23:45 -0300

    Initial commit with clion project

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  This algorithm tries to find the shortest path between the "&" and "#" where all 0's are possible ways and
+ *  all 1's are obstacles.
+ *
+ *  input example:
+ *
+ *  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 & 0 0 0 0 0 1 0 0 0 0 0 0 # 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+ *
+ * @return
+ */
+int main() {
+    printf("A*");
+    return 0;
+}
